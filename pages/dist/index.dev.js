4e4958b12703bde7263e42baf71eb442151202c5
Olga T <jenflower97@gmail.com>
2021-08-05 23:54:07 +0400

    created 1920px display and header animations

--- pages/dist/index.dev.js
+++ pages/dist/index.dev.js
@@ -15,6 +15,15 @@ var filterInput = _toConsumableArray(document.querySelectorAll('.filter__input')
 
 var main = document.querySelector('.main');
 var header = document.querySelector('.header');
+
+var vacations = _toConsumableArray(document.querySelectorAll('.content__item'));
+
+function vacationHover(item, mouse, colorName) {
+  item.querySelector('.content__vacation').addEventListener(mouse, function (evt) {
+    evt.target.style.color = colorName;
+  });
+}
+
 switcher.addEventListener('click', function (evt) {
   console.log('click');
   evt.target.classList.toggle('switch-block__switcher_on');
@@ -24,12 +33,24 @@ switcher.addEventListener('click', function (evt) {
       item.style.backgroundColor = "#19202D";
       item.style.color = "#fff";
     });
+    vacations.forEach(function (item) {
+      item.querySelector('.content__vacation').style.color = "#fff";
+      item.style.backgroundColor = "#19202D";
+      vacationHover(item, 'mouseover', "#6E8098");
+      vacationHover(item, 'mouseout', "#fff");
+    });
     main.style.backgroundColor = "#121721";
     header.style.backgroundColor = "#121721";
   } else {
     filterInput.forEach(function (item) {
       item.style.backgroundColor = "#fff";
       item.style.color = "#19202D";
+    });
+    vacations.forEach(function (item) {
+      item.querySelector('.content__vacation').style.color = "#19202D";
+      item.style.backgroundColor = "#fff";
+      vacationHover(item, 'mouseover', "#6E8098");
+      vacationHover(item, 'mouseout', "#19202D");
     });
     main.style.backgroundColor = "#F4F6F8";
     header.style.backgroundColor = "#F4F6F8";
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ const filter = document.querySelector('.filter')
 const filterInput = [...document.querySelectorAll('.filter__input')]
 const main = document.querySelector('.main')
 const header = document.querySelector('.header')
+const vacations = [...document.querySelectorAll('.content__item')]
 
+function vacationHover(item, mouse, colorName) {
+    item.querySelector('.content__vacation').addEventListener(mouse, (evt) => {
+        evt.target.style.color = colorName
+    })
+}
 
 switcher.addEventListener('click', (evt) => {
     console.log('click')
@@ -14,7 +20,13 @@ switcher.addEventListener('click', (evt) => {
             item.style.backgroundColor = "#19202D"
             item.style.color = "#fff"
         })
+        vacations.forEach(item => {
+            item.querySelector('.content__vacation').style.color = "#fff"
+            item.style.backgroundColor = "#19202D"
 
+            vacationHover(item, 'mouseover', "#6E8098")
+            vacationHover(item, 'mouseout', "#fff")
+        })
         main.style.backgroundColor = "#121721"
         header.style.backgroundColor = "#121721"
         
@@ -22,6 +34,13 @@ switcher.addEventListener('click', (evt) => {
         filterInput.forEach(item => {
             item.style.backgroundColor = "#fff"
             item.style.color = "#19202D"
+        })
+        vacations.forEach(item => {
+            item.querySelector('.content__vacation').style.color = "#19202D"
+            item.style.backgroundColor = "#fff"
+
+            vacationHover(item, 'mouseover', "#6E8098")
+            vacationHover(item, 'mouseout', "#19202D")
         })
 
         main.style.backgroundColor = "#F4F6F8"
